ab98377f36fa475d070359b6d6f940de2220990a
Anders Lewis <greagle3@gmail.com>
2016-11-18 23:59:22 -0800

    Fix pathing on install.sh

--- install.sh
+++ install.sh
@@ -8,29 +8,29 @@ mkdir -p $wd/backups
 # Copy files over:
 # Dotfilewm (Just copied for ease in pathing):
 [ -e ~/.dotfilewm ] && mv ~/.dotfilewm $wd/backups 
-cp -r $wd/dotfilewm ~/.dotfilewm
+cp -r ./dotfilewm ~/.dotfilewm
 
 # xinitrc
 [ -e ~/.xinitrc ] && mv ~/.xinitrc $wd/backups
-ln $wd/xinitrc ~/.xinitrc
+ln ./xinitrc ~/.xinitrc
 
 # Xdefaults
 [ -e ~/.Xdefaults ] && mv ~/.Xdefaults $wd/backups
-ln $wd/Xdefaults ~/.Xdefaults
+ln ./Xdefaults ~/.Xdefaults
 
 # Vimrc
 [ -e ~/.vimrc ] && mv ~/.vimrc $wd/backups
-ln $wd/vimrc ~/.vimrc
+ln ./vimrc ~/.vimrc
 
 # sxhkd:
 mkdir -p ~/.config/sxhkd
 [ -e ~/.config/sxhkd/sxhkdrc ] && mv ~/.config/sxhkd/sxhkdrc $wd/backups
-ln  $wd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+ln ./sxhkdrc ~/.config/sxhkd/sxhkdrc
 
 # fish:
 [ -e ~/.Xdefaults ] && mkdir -p ~/.config/fish
 [ -e ~/.config/fish/config.fish ] && mv ~/.config/fish/config.fish $wd/backups
-ln  $wd/config.fish ~/.config/fish/config.fish
+ln  ./config.fish ~/.config/fish/config.fish
 
 # Install the wm if it's not installed yet.
 ~/.dotfilewm/install.sh
