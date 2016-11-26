803b579d36b580ad3d73c1d2f6fdf8bcfd2037e9
Anders Lewis <greagle3@gmail.com>
2016-11-26 01:33:27 -0800

    Add mouse window movement

--- dotfilewm/install.sh
+++ dotfilewm/install.sh
@@ -7,11 +7,3 @@ make
 mv colors ../looks
 cd ..
 rm -rf colors
-
-# Get alt+tab and focus manager
-git clone https://github.com/patrickhaller/no-wm.git
-cd no-wm
-make
-mv x-focus-manager ../core
-cd ..
-rm -rf no-wm
