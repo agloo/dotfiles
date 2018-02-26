79b1ecc5af5eced8e2c28fb4edc88058e77e2b20
Anders Lewis <greagle3@gmail.com>
2018-02-25 18:49:28 -0800

    Clean up and document Thinkpad section.

--- thinkpad/flip_touch.sh
+++ thinkpad/flip_touch.sh
@@ -7,34 +7,66 @@
 rotation="$(xrandr -q --verbose | grep 'connected' | egrep -o  '\) (normal|left|inverted|right) \(' | egrep -o '(normal|left|inverted|right)')"
 
 # Using current screen orientation proceed to rotate screen and input devices.
-
-case "$rotation" in
-    normal)
-    # rotate to the left
-    xrandr -o inverted
-    xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate half
-    xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate half
-    xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate half
-    ;;
-    left)
-    # rotate back to normal
-    xrandr -o normal
-    xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate none
-    xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate none
-    xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate none
-    ;;
-    inverted)
-    # rotate to the right
-    xrandr -o normal
-    xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate none
-    xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate none
-    xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate none
-    ;;
-    right)
-    # rotate to normal
-    xrandr -o normal
-    xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate none
-    xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate none
-    xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate none
-    ;;
-esac
+if [[ $1 == rot ]];then
+	case "$rotation" in
+			normal)
+			# rotate to the left
+			xrandr -o left
+			xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate ccw
+			xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate ccw
+			xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate ccw
+			;;
+			left)
+			# rotate back to normal
+			xrandr -o normal
+			xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate none
+			xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate none
+			xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate none
+			;;
+			inverted)
+			# rotate to the right
+			xrandr -o right
+			xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate cw
+			xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate cw
+			xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate cw
+			;;
+			right)
+			# rotate to normal
+			xrandr -o normal
+			xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate none
+			xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate none
+			xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate none
+			;;
+	esac
+	else #Default behaviour is to flip.
+	case "$rotation" in
+			normal)
+			# rotate to the left
+			xrandr -o inverted
+			xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate half
+			xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate half
+			xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate half
+			;;
+			left)
+			# rotate back to normal
+			xrandr -o normal
+			xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate none
+			xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate none
+			xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate none
+			;;
+			inverted)
+			# rotate to the right
+			xrandr -o normal
+			xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate none
+			xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate none
+			xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate none
+			;;
+			right)
+			# rotate to normal
+			xrandr -o normal
+			xsetwacom set "Wacom ISDv4 E6 Pen stylus" rotate none
+			xsetwacom set "Wacom ISDv4 E6 Pen eraser" rotate none
+			xsetwacom set "Wacom ISDv4 E6 Finger touch" rotate none
+			;;
+	esac
+fi
---
+++ thinkpad/readme.txt
@@ -0,0 +1,4 @@
+These scripts are thinkpad specific and keybindings source directly to this directory
+(assuming the dotfiles dir is in your XDG home.)
+If you want the WM but don't care about rotating your screen or changing your volume
+with hardware buttons you can safely delete this dir and everything under [Thinkpad] in sxxhkd.
